071d18ca3efd532533202e1096c59c28aea49da2
SebRainDeer <sebastian@de-mare.se>
2026-01-28 10:57:25 +0100

    made connection to reddit

--- bot/reddit_finder.py
+++ bot/reddit_finder.py
@@ -4,8 +4,8 @@ url = "https://www.reddit.com/r/mildlyinteresting/.json"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/114.0.0.0 Safari/537.36"
+              "AppleWebKit/537.36 (KHTML, like Gecko) "
+              "Chrome/114.0.0.0 Safari/537.36"
 }
 
 response = requests.get(url, headers=headers)
@@ -21,4 +21,26 @@ if response.status_code == 200:
 else:
     print("Failed to get JSON. Response text:")
     print(response.text[:500])  # show first 500 characters
-    print('hej')
+
+def show500():
+    url = "https://www.reddit.com/r/mildlyinteresting/.json"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/114.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+    print("Status code:", response.status_code)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        for post in posts:
+            title = post["data"]["title"]
+            url_post = post["data"]["url"]
+            return title, "-", url_post
+    else:
+        return "Failed to get JSON. Response text:"
+        return response.text[:500]  # show first 500 characters
--- bot/top_posts.py
+++
@@ -1,4 +0,0 @@
-from reddit_connector import redditconnect
-
-for submission in redditconnect.subreddit('mildyinteresting').top(limit=10):
-    print(submission.title)
