ad0af9ceb131fcfb614465f7ed42e97f6228e4d2
SebRainDeer <sebastian@de-mare.se>
2026-02-02 17:36:32 +0100

    small changes

--- bot/reddit_children_finder.py
+++ bot/reddit_children_finder.py
@@ -1,15 +1,24 @@
+import requests
 from bot.reddit_connector import reddit_posts
 from json_storage.json_loader import save_titles
 
 #this module will take in name of reddit posts and the url link of them in a list
 
-print(reddit_posts())
-
-
+#print(reddit_posts())
 
 def child_url_post():
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/114.0.0.0 Safari/537.36"
+    }
     posts = reddit_posts()
     for post in posts:
+        response = requests.get(post['url'], headers)
+        if response.status_code == 200:
+            data = response.json()
+            upvotes = data['ups']
+            print(upvotes)
         print(post['url'])
         print(post['title'])
 
